9b62453d355b057b2c8ebd1bc85d04a995a346df
Pouria <82539162+Pouria1378@users.noreply.github.com>
2022-07-08 19:23:37 +0430

    change heroku app (server) url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        API_URL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : 'https://test-heroku-peaky.herokuapp.com/',
+        API_URL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : 'https://peaky-backend-pouria-seifi.herokuapp.com/',
     },
 }

316ad5279b19e8e9005d75e113d63c24dbebe1dc
Pouria <82539162+Pouria1378@users.noreply.github.com>
2024-02-11 16:25:54 +0330

    change next.config and use API_URL in it from envs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     env: {
-        API_URL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : 'https://peaky-backend-pouria-seifi.herokuapp.com/',
+        API_URL: process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:8000'
+            : process.env.API_URL,
     },
 }
